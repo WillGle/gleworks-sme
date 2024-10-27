e9d6dbe86f25b629ab6c2323e64793cb6d3430d7
Lê Bá Đắc <141981497+lebadac@users.noreply.github.com>
2024-10-27 09:33:55 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/LoginSignupPassword/Login";
 import Signup from "./components/LoginSignupPassword/Signup";
 import LostPass from "./components/LoginSignupPassword/LostPass";
-import ConfirmationSuccess from './components/ConfirmationSuccess';
-import AccountConfirmation from './components/AccountConfirmation';
 
 function App() {
   return (
@@ -13,8 +11,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/lost-password" element={<LostPass />} />
-        <Route path="/confirmation-success" element={<ConfirmationSuccess />} />
-        <Route path="/" element={<AccountConfirmation />} />
         {/* Thêm tuyến đường mặc định để điều hướng tới /login */}
         <Route path="*" element={<Login />} />
       </Routes>

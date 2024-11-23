99514be6b42d73ad0937c7acf25524b49c95377b
lebadac <21521911@gm.uit.edu.vn>
2024-11-23 22:35:40 +0700

    fix: Remove proctected route for kind of service; feat: update role when login

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,13 +7,14 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const token = localStorage.getItem("token"); // Kiểm tra token trong localStorage
+  const role = localStorage.getItem("role"); // Lấy role từ localStorage
 
-  if (!token) {
-    // Nếu không có token, chuyển hướng đến trang đăng nhập
+  if (!token || role !== 'user') {
+    // Nếu không có token hoặc role không phải là 'user', chuyển hướng đến trang đăng nhập
     return <Navigate to="/login" />;
   }
 
-  return <>{children}</>; // Nếu có token, hiển thị nội dung con
+  return <>{children}</>; // Nếu có token và role là 'user', hiển thị nội dung con
 };
 
 export default ProtectedRoute;

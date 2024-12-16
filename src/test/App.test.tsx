ab7eeb6d2655bd6b63b10c27c3d2a1e8be60b862
Gleammy <cuongsayyay@gmail.com>
2024-12-16 14:23:13 +0700

    test github action

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,8 +1,7 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "../App";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-// test("renders the app", () => {
-//   render(<App />);
-//   expect(screen.getByText("GLE.WORK")).toBeInTheDocument();
-// });
-//
+test("renders the app", () => {
+  render(<App />);
+  expect(screen.getByText("GLE.WORK")).toBeInTheDocument();
+});

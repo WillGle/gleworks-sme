10129ef2450e66cecbadbc0f660b1015effb300c
WillGle <cuongsayyay@gmail.com>
2026-03-15 15:12:37 +0700

    chore: exclude .direnv from vitest matching to avoid nix file duplicates

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,13 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp,direnv}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*'
+    ],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
